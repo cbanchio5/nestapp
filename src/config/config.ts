09fb5c0c7381130d7e9103daceff5f9b1941930e
Cesar Banchio <cesarbanchio@air-de-cesar.home>
2024-05-29 15:16:22 +0200

    added email services

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 export const config = () => ({
     port: process.env.PORT || 3000,
     sendgridApiKey : process.env.SENDGRID_API_KEY, 
-    sendgridApi: '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'
+    sendgridApi: process.env.SENDGRID_API_KEY
   })
